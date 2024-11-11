7424b905079789d0db02cc21580d3c3fbc3a27bb
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-11-11 18:36:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    double a , b ;
+    cin >> a >> b ;
+    cout << fixed << setprecision(9) << log(b)/log(a) ;
+
+    return 0;
+}
